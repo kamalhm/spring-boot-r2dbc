134498321e0e4c9572ce2bc89725ba30372f9ded
Kamal Mahmud <kamalhm1997@gmail.com>
2021-04-13 15:54:49 +0700

    feat: add auditor

--- src/main/java/com/khm/reactivepostgres/Balance.java
+++ src/main/java/com/khm/reactivepostgres/Balance.java
@@ -2,15 +2,33 @@ package com.khm.reactivepostgres;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.relational.core.mapping.Column;
+
+import java.util.UUID;
 
 @Builder
 @Data
 public class Balance {
   @Id
-  private String id;
+  private UUID id;
   @Column("member_id")
   private String memberId;
   private Long balance;
+  @CreatedBy
+  @Column("created_by")
+  private String createdBy;
+  @CreatedDate
+  @Column("created_date")
+  private Long createdDate;
+  @LastModifiedBy
+  @Column("last_modified_by")
+  private String lastModifiedBy;
+  @LastModifiedDate
+  @Column("last_modified_date")
+  private Long lastModifiedDate;
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,5 +8,10 @@ CREATE TABLE IF NOT EXISTS member (
 CREATE TABLE IF NOT EXISTS balance (
     id uuid default uuid_generate_v4(),
     member_id TEXT NOT NULL,
-    balance integer NOT NULL
+    balance integer NOT NULL,
+    created_by TEXT NOT NULL,
+    created_date bigint NOT NULL,
+    last_modified_date bigint NOT NULL,
+    last_modified_by TEXT NOT NULL,
+    updated_by TEXT
 );
