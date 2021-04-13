eb23f19746ab80531732ba8340633ade81f6b39c
Kamal Mahmud <kamalhm1997@gmail.com>
2021-04-13 14:45:44 +0700

    refactor: change id to uuid

--- src/main/java/com/khm/reactivepostgres/Balance.java
+++ src/main/java/com/khm/reactivepostgres/Balance.java
@@ -1,11 +1,16 @@
 package com.khm.reactivepostgres;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
+@Builder
 @Data
 public class Balance {
   @Id
   private String id;
+  @Column("member_id")
+  private String memberId;
   private Long balance;
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS member (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS balance (
-    id TEXT PRIMARY KEY,
+    id uuid default uuid_generate_v4(),
+    member_id TEXT NOT NULL,
     balance integer NOT NULL
 );
